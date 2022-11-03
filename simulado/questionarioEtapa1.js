10c5ffa01509545dc6a9160393ddcb3818b82097
Rafael202074 <rafael202074@gmail.com>
2022-11-02 22:36:51 -0300

    Criada inserção de html e questionario dinamico, pastas banco e simulado

---
+++ simulado/questionarioEtapa1.js
@@ -0,0 +1,18 @@
+var questionario = [
+    {
+        questao: '1) Qual o comando utilizado para a criação de um banco de dados?',
+        a: 'A) ALTER TABLE',
+        b: 'B) ALTER COLUMN',
+        c: 'C) CREATE TABLE',
+        d: 'D) CREATE DATABASE',
+        resposta: 'D'
+    },
+    {
+        questao: '2) Qual o comando utilizado para a criação de tabelas?',
+        a: 'A) ALTER TABLE',
+        b: 'B) ALTER COLUMN',
+        c: 'C) CREATE TABLE',
+        d: 'D) CREATE DATABASE',
+        resposta: 'C'
+    },
+]
