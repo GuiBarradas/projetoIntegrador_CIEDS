9c5c3ff2b45280a8547f17a6f84fa4cbcb4530fb
Guilherme <guilhermebarradasdev@gmail.com>
2022-11-03 14:13:46 -0300

    adicionando etapa 2 js

--- banco/etapas.js
+++ banco/etapas.js
@@ -23,5 +23,29 @@ var etapas =
             facilitar o preenchimento e evitar erros.
             #*
             imagem:assets/etapa1.png
+        `,
+
+        etapa2:
         `
+        topico:1 - Criar tela de Filtro e função de Cadastro de Dados:
+        #*
+        subtopico: Criar uma 'procedure' para armazenar os dados da 1º etapa no banco de dados pela tela do formulário.
+        #*
+        video:https://www.youtube.com/embed/1SOk2p1peLo
+        #*
+        Obs: (Há um modelo pronto de procedure que segue o padrão da empresa disponibilizado em nosso servidor do Discord na aba "exemplos-iniciais")
+        #*
+        topico:2 - Filtrar os dados e exclusão lógica
+        #*
+        subtopico:A tela também deverá ser capaz de recuperar todos os dados anteriormente gravados e excluir-los logicamente (Inativar o registro).
+        #*
+        topico:3 - Construir tela individual para o filtro
+        #*
+        subtopico:Construir uma tela de filtro apresentando: Nome, data de nascimento, ativo e CPF.
+        <br>
+        Na tela de filtro deverá ser possível pesquisar por nome, ativo, CPF e data de nascimento.
+        #*
+        imagem:assets/etapa2.png
+    `
+        
     }
