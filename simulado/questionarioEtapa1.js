d70ade003e37f29cc70ef787d4b4511caa094d4a
Guilherme Barradas <87228787+GuiBarradas@users.noreply.github.com>
2022-11-06 02:20:36 -0300

    Revert "Rafael"

--- simulado/questionarioEtapa1.js
+++ simulado/questionarioEtapa1.js
@@ -1,39 +1,26 @@
-var questionario = {
-    etapa1:[
-        {
-            questao: '1) Qual o comando utilizado para a criação de um banco de dados?',
-            a: 'A) ALTER TABLE',
-            b: 'B) ALTER COLUMN',
-            c: 'C) CREATE TABLE',
-            d: 'D) CREATE DATABASE',
-            resposta: 'D'
-        },
-        {
-            questao: '2) Qual o comando utilizado para a criação de tabelas?',
-            a: 'A) ALTER TABLE',
-            b: 'B) ALTER COLUMN',
-            c: 'C) CREATE TABLE',
-            d: 'D) CREATE DATABASE',
-            resposta: 'C'
-        },
-        {
-            questao: '3) Qual o comando utilizado para recuperar os dados de um objeto do banco de dados, como uma tabela, view e, em alguns casos, uma stored procedure?',
-            a: 'A) SELECT',
-            b: 'B) RETRIEVE',
-            c: 'C) WHERE',
-            d: 'D) HAVING',
-            resposta: 'A'
-        }
-    ],
-    
-    etapa2:[
-        {
-            questao: '1) Com quantas marmotas se faz uma foca?',
-            a: 'A) ????????',
-            b: 'B) Acho que letra D',
-            c: 'C) Acho que letra B',
-            d: 'D) Talvez A',
-            resposta: 'C'
-        },
-    ],
-}
+var questionario = [
+    {
+        questao: '1) Qual o comando utilizado para a criação de um banco de dados?',
+        a: 'A) ALTER TABLE',
+        b: 'B) ALTER COLUMN',
+        c: 'C) CREATE TABLE',
+        d: 'D) CREATE DATABASE',
+        resposta: 'D'
+    },
+    {
+        questao: '2) Qual o comando utilizado para a criação de tabelas?',
+        a: 'A) ALTER TABLE',
+        b: 'B) ALTER COLUMN',
+        c: 'C) CREATE TABLE',
+        d: 'D) CREATE DATABASE',
+        resposta: 'C'
+    },
+    {
+        questao: '3) Qual o comando utilizado para recuperar os dados de um objeto do banco de dados, como uma tabela, view e, em alguns casos, uma stored procedure?',
+        a: 'A) SELECT',
+        b: 'B) RETRIEVE',
+        c: 'C) WHERE',
+        d: 'D) HAVING',
+        resposta: 'A'
+    },
+]
